21ccb42d81c8515adb310a4fbf50517b3520495d
RGBboy <l-_-l@rgbboy.com>
2014-03-07 14:19:35 +1300

    Added tests for bigBlock.get; Updated BigBlock to pass tests;

--- index.js
+++ index.js
@@ -6,7 +6,12 @@
  * Module Dependencies
  */
 
-var BigBlock;
+var BigBlock,
+    di = require('di'),
+    EntitySystem = require('./lib/entity-system'),
+    Entity = require('./lib/entity'),
+    ComponentSystem = require('./lib/component-system'),
+    Component = require('./lib/component');
 
 /**
  * BigBlock
@@ -14,9 +19,28 @@ var BigBlock;
  * @return {Game}
  * @api public
  */
-BigBlock = function () {
-  
+BigBlock = function (modules) {
+
+  var self = {},
+      coreModules = [EntitySystem, ComponentSystem, Component, Entity],
+      gameModules,
+      injector;
+
+  if (modules) {
+    coreModules = coreModules.concat(modules);
+  };
+
+  injector = new di.Injector(coreModules);
+
+  self.get = function (token) {
+    return injector.get(token);
+  };
+
+  return self;
+
 };
+
+BigBlock.EntitySystem = EntitySystem;
 
 /**
  * Module Exports
--- test/big-block.spec.js
+++ test/big-block.spec.js
@@ -8,15 +8,15 @@
 
 var test = require('tape'),
     sinon = require('sinon'),
-    BigBlock = require('../index'),
-    game;
+    di = require('di'),
+    BigBlock = require('../index');
 
 /**
  * Setup
  */
 
 var setup = function (t) {
-  game = BigBlock();
+  
 };
 
 /**
@@ -24,6 +24,7 @@ var setup = function (t) {
  */
 
 var teardown = function (t) {
+  
 };
 
 /**
@@ -33,4 +34,36 @@ var teardown = function (t) {
 test('BigBlock', function (t) {
     t.plan(1);
     t.ok(BigBlock, 'class should exist');
+});
+
+test('BigBlock.EntitySystem', function (t) {
+    t.plan(1);
+    t.ok(BigBlock.EntitySystem, 'class should exist');
+});
+
+/**
+ * bigBlock.get
+ */
+
+test('bigBlock.get should be a function', function (t) {
+  var game = BigBlock();
+  t.plan(1);
+  t.equal(typeof game.get, 'function');
+});
+
+test('bigBlock.get should return a instantiated singleton', function (t) {
+  var game,
+      testSystem;
+
+  function TestSystem (entitySystem) {
+    this.entitySystem = entitySystem;
+  };
+
+  t.plan(3);
+  di.annotate(TestSystem, new di.InjectAnnotation(BigBlock.EntitySystem));
+  game = BigBlock([TestSystem]);
+  testSystem = game.get(TestSystem);
+  t.ok(testSystem instanceof TestSystem, 'testSystem is an instance of TestSystem');
+  t.ok(testSystem.entitySystem instanceof BigBlock.EntitySystem, 'Injected System is correct instance')
+  t.pass();
 });
